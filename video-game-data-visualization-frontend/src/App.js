aa55a6ce39528cfe213b6329a8ab7129fbe4ff5e
Mario Lugo <mlugo23699@gmail.com>
2022-10-06 08:18:44 -0700

    Generated component folder and created PlatformStats file

--- video-game-data-visualization-frontend/src/App.js
+++ video-game-data-visualization-frontend/src/App.js
@@ -1,7 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { useEffect, useState } from 'react';
+import PlatformStats from './components/PlatformStats';
 
 function App() {
+
+  const [videoGames, setVideoGames] = useState([]);
+
+  useEffect(() => {
+    getVideoGames();
+  }, []);
+
+  async function getVideoGames() {
+    try{
+      debugger;
+      const response = await axios.get("http://localhost:8080/games");
+      console.log(response.data)
+      setVideoGames(response.data);
+
+    } catch(error){
+      console.log(error.response)
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +40,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <div>
+          <PlatformStats videoGames = {videoGames}/>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ video-game-data-visualization-frontend/src/components/PlatformStats.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+import { Chart } from "react-google-charts";
+
+
+const PlatformStats = () => {
+    return null;
+}
+export default PlatformStats
